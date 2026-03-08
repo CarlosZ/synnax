3cf3e8c8f505341c10bcfc419297d4e3053f661f
Carlos Zuniga <carlosluiszuniga@gmail.com>
2026-03-07 16:39:18 -0800

    Configures diffview.nvim
    Updates diffview.nvim to a maintained fork.

--- neovim/plugins/diffview.nix
+++ neovim/plugins/diffview.nix
@@ -1,7 +1,56 @@
-{ lib, isDev, ... }:
+{
+  pkgs,
+  lib,
+  isDev,
+  ...
+}:
+let
+  diffviewNvimPkg =
+    (pkgs.vimUtils.buildVimPlugin {
+      name = "diffview-nvim";
+      src = pkgs.fetchFromGitHub {
+        owner = "dlyongemallo";
+        repo = "diffview.nvim";
+        tag = "v0.21";
+        hash = "sha256-mlp7NQhnPD4uJlP7foIhD5EvQBrVHH4tLgXqy7IvgsI=";
+      };
+    }).overrideAttrs
+      (_: {
+        name = "diffview-nvim";
+
+        nvimSkipModules = [
+          "diffview.api.views.diff.diff_view"
+          "diffview.job"
+          "diffview.multi_job"
+          "diffview.scene.layouts.diff_2"
+          "diffview.scene.layouts.diff_2_hor"
+          "diffview.scene.layouts.diff_2_ver"
+          "diffview.scene.layouts.diff_3"
+          "diffview.scene.layouts.diff_3_hor"
+          "diffview.scene.layouts.diff_3_mixed"
+          "diffview.scene.layouts.diff_3_ver"
+          "diffview.scene.layouts.diff_4"
+          "diffview.scene.layouts.diff_4_mixed"
+          "diffview.scene.views.diff.diff_view"
+          "diffview.scene.views.file_history.file_history_panel"
+          "diffview.scene.views.file_history.option_panel"
+          "diffview.scene.window"
+          "diffview.ui.panel"
+          "diffview.ui.panels.commit_log_panel"
+          "diffview.ui.panels.help_panel"
+          "diffview.vcs.adapter"
+          "diffview.vcs.adapters.git.init"
+          "diffview.vcs.adapters.hg.init"
+          "diffview.vcs.adapters.jj.init"
+          "diffview.vcs.adapters.p4.init"
+        ];
+
+        doInstallCheck = true;
+      });
+in
 {
   vim = lib.mkIf isDev {
-    # TODO: Replace with dlyongemallo/diffview.nvim
+    lazy.plugins.diffview-nvim.package = lib.mkForce diffviewNvimPkg;
     utility.diffview-nvim = {
       enable = true;
     };
