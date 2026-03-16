5ba3d9f57d9d6ba26d4d8728e05af79f94be7c8f
Carlos Zuniga <carlosluiszuniga@gmail.com>
2026-03-16 11:40:11 -0700

    Enables trouble.nvim and fixes code keymaps

--- neovim/plugins/lsp.nix
+++ neovim/plugins/lsp.nix
@@ -1,10 +1,4 @@
 { lib, config, ... }:
-let
-  inherit (lib.generators) mkLuaInline;
-  inherit (lib.nvim.dag) entryBefore;
-
-  augroup = "synnax_lsp";
-in
 {
   vim = {
     lsp = {
@@ -14,34 +8,13 @@ in
       mappings = lib.mapAttrs (_: _: null) config.vim.lsp.mappings;
       lightbulb.enable = true;
       lspkind.enable = true;
+      trouble = {
+        enable = true;
+        mappings = lib.mapAttrs (_: _: null) (
+          lib.filterAttrs (k: _: k != "toggle") config.vim.lsp.trouble.mappings
+        );
+      };
     };
-
-    augroups = [ { name = augroup; } ];
-    autocmds = [
-      {
-        group = augroup;
-        event = [ "LspAttach" ];
-        desc = "LSP on-attach add custom keybinds";
-        callback = mkLuaInline ''
-          function(event)
-            local bufnr = event.buf
-            local client = vim.lsp.get_client_by_id(event.data.client_id)
-            attach_keymap_groups(client, bufnr)
-          end
-        '';
-      }
-    ];
-
-    luaConfigRC.lspkeymapgroups = entryBefore [ "lsp-setup" "autocmds" ] ''
-      local attach_keymap_groups = function()
-        local wk = require("which-key")
-        wk.add({
-          { "<leader>cx", desc = "+Diagnostics", icon = { icon = "󰩂", color = "blue" } },
-          { "<leader>cxe", desc = "+Errors", icon = { icon = "", color = "red" } },
-          { "<leader>cxw", desc = "+Warnings", icon = { icon = "", color = "yellow" } },
-        })
-      end
-    '';
   };
 
   synnax.keys = {
@@ -51,39 +24,24 @@ in
       iconColor = "orange";
       maps = [
         {
-          key = "D";
-          mode = [ "n" ];
-          action = "vim.lsp.buf.declaration";
-          desc = "Go to declaration";
-          lua = true;
-          has = "declaration";
-        }
-
-        {
-          key = "d";
-          mode = [ "n" ];
-          action = "vim.lsp.buf.definition";
-          desc = "Go to definition";
-          lua = true;
-          has = "definition";
-        }
-
-        {
-          key = "y";
-          mode = [ "n" ];
-          action = "vim.lsp.buf.type_definition";
-          desc = "Go to type";
-          lua = true;
-          has = "typeDefinition";
-        }
-
-        {
           key = "i";
           mode = [ "n" ];
-          action = "vim.lsp.buf.implementation";
-          desc = "List implementations";
-          lua = true;
-          has = "implementation";
+          action = "<cmd>Trouble lsp toggle focus=false win.position=right<cr>";
+          desc = "List impl / symbos/ refs";
+        }
+
+        {
+          key = "l";
+          mode = [ "n" ];
+          action = "<cmd>Trouble loclist toggle<cr>";
+          desc = "Location list";
+        }
+
+        {
+          key = "q";
+          mode = [ "n" ];
+          action = "<cmd>Trouble quickfix toggle<cr>";
+          desc = "Quickfix list";
         }
 
         {
@@ -96,96 +54,26 @@ in
         }
 
         {
-          key = "xn";
+          key = "n";
           mode = [ "n" ];
           action = "vim.diagnostic.goto_next";
           desc = "Go to next diagnostic";
           lua = true;
-          enabled = ''
-            function(bufnr)
-              return #(vim.diagnostic.get(bufnr)) > 0
-            end
-          '';
         }
 
         {
-          key = "xp";
+          key = "p";
           mode = [ "n" ];
           action = "vim.diagnostic.goto_prev";
           desc = "Go to previous diagnostic";
           lua = true;
-          enabled = ''
-            function(bufnr)
-              return #(vim.diagnostic.get(bufnr)) > 0
-            end
-          '';
-        }
-
-        {
-          key = "xen";
-          mode = [ "n" ];
-          action = "function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR }) end";
-          desc = "Next error";
-          lua = true;
-          enabled = ''
-            function(bufnr)
-              return #(vim.diagnostic.get(bufnr)) > 0
-            end
-          '';
-        }
-
-        {
-          key = "xep";
-          mode = [ "n" ];
-          action = "function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR }) end";
-          desc = "Previous error";
-          lua = true;
-          enabled = ''
-            function(bufnr)
-              return #(vim.diagnostic.get(bufnr)) > 0
-            end
-          '';
-        }
-
-        {
-          key = "xwn";
-          mode = [ "n" ];
-          action = "function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN }) end";
-          desc = "Next warning";
-          lua = true;
-          enabled = ''
-            function(bufnr)
-              return #(vim.diagnostic.get(bufnr)) > 0
-            end
-          '';
-        }
-
-        {
-          key = "xwp";
-          mode = [ "n" ];
-          action = "function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN }) end";
-          desc = "Previous warning";
-          lua = true;
-          enabled = ''
-            function(bufnr)
-              return #(vim.diagnostic.get(bufnr)) > 0
-            end
-          '';
         }
 
         {
           key = "e";
           mode = [ "n" ];
-          action = "vim.diagnostic.open_float";
-          desc = "Open diagnostic float";
-          lua = true;
-          enabled = ''
-            function(bufnr)
-              vim.print("bufnr: " .. bufnr)
-              vim.print("diags: " .. #(vim.diagnostic.get(bufnr)))
-              return #(vim.diagnostic.get(bufnr)) > 0
-            end
-          '';
+          action = "<cmd>Trouble diagnostics focus=true<cr>";
+          desc = "Open diagnostics";
         }
 
         {
@@ -200,9 +88,8 @@ in
         {
           key = "s";
           mode = [ "n" ];
-          action = "vim.lsp.buf.document_symbol";
+          action = "<cmd>Trouble symbols toggle focus=false<cr>";
           desc = "List document symbols";
-          lua = true;
           has = "documentSymbol";
         }
 
