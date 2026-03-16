89f3a5adc3d038128c6a38536578e5f91ba45070
Carlos Zuniga <carlosluiszuniga@gmail.com>
2026-03-16 16:52:33 -0700

    Fixes formatting

--- neovim/keymaps.nix
+++ neovim/keymaps.nix
@@ -1,8 +1,4 @@
-{
-  lib,
-  config,
-  ...
-}:
+{ lib, config, ... }:
 let
   inherit (lib.generators) mkLuaInline;
   inherit (lib.options) mkOption literalMD;
--- nix/synnax.nix
+++ nix/synnax.nix
@@ -1,29 +1,31 @@
-{inputs, ...}: {
-  perSystem = {
-    pkgs,
-    inputs',
-    ...
-  }: let
-    mkPackage = pkgs.callPackage (
-      {flavor}:
+{ inputs, ... }:
+{
+  perSystem =
+    { pkgs, inputs', ... }:
+    let
+      mkPackage = pkgs.callPackage (
+        { flavor }:
         (inputs.nvf.lib.neovimConfiguration {
           inherit pkgs;
-          modules = [../neovim];
-          extraSpecialArgs = let
-            isDev = flavor == "dev";
-            isMin = flavor == "min";
-          in {
-            inherit isDev isMin;
-            inherit inputs';
-            flakeInputs = inputs;
-          };
+          modules = [ ../neovim ];
+          extraSpecialArgs =
+            let
+              isDev = flavor == "dev";
+              isMin = flavor == "min";
+            in
+            {
+              inherit isDev isMin;
+              inherit inputs';
+              flakeInputs = inputs;
+            };
         }).neovim
-    );
-  in {
-    packages = rec {
-      default = dev;
-      dev = mkPackage {flavor = "dev";};
-      min = mkPackage {flavor = "min";};
+      );
+    in
+    {
+      packages = rec {
+        default = dev;
+        dev = mkPackage { flavor = "dev"; };
+        min = mkPackage { flavor = "min"; };
+      };
     };
-  };
 }
